a5bfd042ac5412d5a9119bf95eb4505af5961efa
RaphaelaMulthaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2025-03-10 20:59:25 +0100

    exchange 'currentLanguageButtonImgSet'

--- src/app/shared/mini-menu/mini-menu.component.ts
+++ src/app/shared/mini-menu/mini-menu.component.ts
@@ -6,7 +6,7 @@ import { PortfolioService } from '../../portfolio.service';
   selector: 'app-mini-menu',
   imports: [TranslatePipe, TranslateDirective],
   templateUrl: './mini-menu.component.html',
-  styleUrl: './mini-menu.component.scss'
+  styleUrl: './mini-menu.component.scss',
 })
 export class MiniMenuComponent {
   portfolioService = inject(PortfolioService);
@@ -16,47 +16,70 @@ export class MiniMenuComponent {
   languageIsHovered: boolean = false;
   /** A boolean that indicates whether the burger menu is hovered. */
   burgermenuIsHovered: boolean = false;
+  /** The variable specifies which image set is used for the language button. */
+  currentLanguageButtonImgSet: 'imagesEN' | 'imagesDE' = 'imagesDE';
 
-  /** The paths to different colored language change buttons. */
-  public imagesLanguage: { [key: string]: { normal: string; hover: string } } = {
-    'black': {
+  /** The paths to different colored language change to EN buttons. */
+  public imagesEN: { [key: string]: { normal: string; hover: string } } = {
+    black: {
       normal: 'assets/img/EN_black.png',
       hover: 'assets/img/EN_black_hover.png',
     },
-    'cream': {
+    cream: {
       normal: 'assets/img/EN_cream.png',
       hover: 'assets/img/EN_cream_hover.png',
-    }
+    },
   };
-  
+
+  /** The paths to different colored language change to DE buttons. */
+  public imagesDE: { [key: string]: { normal: string; hover: string } } = {
+    black: {
+      normal: 'assets/img/DE_black.png',
+      hover: 'assets/img/DE_black_hover.png',
+    },
+    cream: {
+      normal: 'assets/img/DE_cream.png',
+      hover: 'assets/img/DE_cream_hover.png',
+    },
+  };
+
   /**
    * The paths to different colored burger menu images.
    */
-  public imagesBurgermenu: { [key: string]: { normal: string; hover: string } } = {
-    'black': {
+  public imagesBurgermenu: {
+    [key: string]: { normal: string; hover: string };
+  } = {
+    black: {
       normal: 'assets/img/burgermenu_black.png',
       hover: 'assets/img/burgermenu_hover.png',
     },
-    'cream': {
+    cream: {
       normal: 'assets/img/burgermenu_cream.png',
       hover: 'assets/img/burgermenu_hover.png',
-    }
+    },
   };
 
   /**
    * This function returns a path to an image, depending on the color class of the header and whether the corresponding img tag is hovered or not.
-   * 
+   *
    * @param hoverState A boolean that indicates whether the img tag is hovered or not.
    * @param imageSet The image set matching the img tag.
    * @returns The path to an image.
    */
-  imageSrc(hoverState: boolean, imageSet: { [key: string]: { normal: string; hover: string } }): string {
-    return hoverState 
-      ? imageSet[this.headerClass]?.hover 
+  imageSrc(
+    hoverState: boolean,
+    imageSet: { [key: string]: { normal: string; hover: string } }
+  ): string {
+    return hoverState
+      ? imageSet[this.headerClass]?.hover
       : imageSet[this.headerClass]?.normal;
   }
 
+  /**
+   * This function calls the function that changes the language. In addition, the image set for the 'languageButton' is stored in the variable 'currentLanguageButtonImgSet'.
+   */
   changeLanguage(): void {
     this.portfolioService.changeLanguage();
+    this.currentLanguageButtonImgSet = this.currentLanguageButtonImgSet === 'imagesEN' ? 'imagesDE' : 'imagesEN';
   }
 }
