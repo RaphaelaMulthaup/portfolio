45daa4ae0222b0cbc1741f6fbe80802990923bf3
RaphaelaMulthaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2025-03-19 09:04:58 +0100

    logoName function call only if it is not on the landing page

--- src/app/main-content/shared/logo-name/logo-name.component.ts
+++ src/app/main-content/shared/logo-name/logo-name.component.ts
@@ -1,15 +1,18 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { PortfolioService } from '../../../portfolio.service';
 import { TranslatePipe, TranslateDirective } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-logo-name',
-  imports: [TranslatePipe, TranslateDirective],
+  imports: [TranslatePipe, TranslateDirective, CommonModule],
   templateUrl: './logo-name.component.html',
   styleUrl: './logo-name.component.scss'
 })
 export class LogoNameComponent {
   portfolioService = inject(PortfolioService);
+  /** A boolean that indicates whether LogoNameComponent is included in LandingPageComponent. */
+  @Input() isLandingPage: boolean = false;
 
   /**
    * This function calls the function in the portfolio service that scrolls to the main components and makes it scroll to the landing page.
