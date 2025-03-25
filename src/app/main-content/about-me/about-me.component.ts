426244b623bc62a71c128f0319af30b382f59b05
RaphaelaMulthaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2025-03-25 10:49:08 +0100

    display overlay

--- src/app/main-content/about-me/about-me.component.ts
+++ src/app/main-content/about-me/about-me.component.ts
@@ -3,13 +3,19 @@ import { HeaderComponent } from '../shared/header/header.component';
 import { TranslatePipe, TranslateDirective } from '@ngx-translate/core';
 import { NavBulletsComponent } from '../shared/nav-bullets/nav-bullets.component';
 import { PortfolioService } from '../../portfolio.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about-me',
-  imports: [HeaderComponent, TranslatePipe, TranslateDirective, NavBulletsComponent],
+  imports: [HeaderComponent, TranslatePipe, TranslateDirective, NavBulletsComponent, CommonModule],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
 export class AboutMeComponent {
   portfolioService = inject(PortfolioService);
+  smileyIsHoverd: boolean = false;
+
+  log(){
+    console.log(this.smileyIsHoverd);
+  }
 }
