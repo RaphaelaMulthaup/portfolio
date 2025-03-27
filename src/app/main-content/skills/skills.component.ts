87c259351430f5028eee4ff800aaa2e1cd433b17
RaphaelaMulthaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2025-03-27 08:54:25 +0100

    animation desired skill

--- src/app/main-content/skills/skills.component.ts
+++ src/app/main-content/skills/skills.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/header/header.component';
 import { TranslatePipe, TranslateDirective } from '@ngx-translate/core';
 import { NavBulletsComponent } from '../shared/nav-bullets/nav-bullets.component';
+import { HexagonComponent } from "../shared/hexagon/hexagon.component";
 
 @Component({
   selector: 'app-skills',
@@ -10,7 +11,8 @@ import { NavBulletsComponent } from '../shared/nav-bullets/nav-bullets.component
     TranslatePipe,
     TranslateDirective,
     NavBulletsComponent,
-  ],
+    HexagonComponent
+],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss',
 })
