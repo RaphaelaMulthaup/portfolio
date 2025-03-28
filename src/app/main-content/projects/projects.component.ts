85760ede72948f5f8ad2554fdc46dbb386413a9b
RaphaelaMulthaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2025-03-28 14:24:43 +0100

    projects lower area

--- src/app/main-content/projects/projects.component.ts
+++ src/app/main-content/projects/projects.component.ts
@@ -2,13 +2,18 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/header/header.component';
 import { TranslatePipe, TranslateDirective } from '@ngx-translate/core';
 import { NavBulletsComponent } from '../shared/nav-bullets/nav-bullets.component';
+import { HexagonComponent } from '../shared/hexagon/hexagon.component';
 
 @Component({
   selector: 'app-projects',
-  imports: [HeaderComponent, TranslatePipe, TranslateDirective, NavBulletsComponent],
+  imports: [
+    HeaderComponent,
+    TranslatePipe,
+    TranslateDirective,
+    NavBulletsComponent,
+    HexagonComponent,
+  ],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrl: './projects.component.scss',
 })
-export class ProjectsComponent {
-
-}
+export class ProjectsComponent {}
