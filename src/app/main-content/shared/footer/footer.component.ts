4751a594a95b918c0c3560a32b3c4696dbace686
RaphaelaMulthaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2025-04-09 18:04:29 +0200

    footer

--- src/app/main-content/shared/footer/footer.component.ts
+++ src/app/main-content/shared/footer/footer.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HexagonComponent } from '../hexagon/hexagon.component';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [HexagonComponent, TranslatePipe],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
