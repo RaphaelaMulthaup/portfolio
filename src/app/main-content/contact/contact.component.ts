a3db5989e31ee159927e08cc7ce375d6c806dcb9
RaphaelaMulthaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2025-04-09 14:03:24 +0200

    structure contact.component

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/header/header.component';
 import { TranslatePipe, TranslateDirective } from '@ngx-translate/core';
 import { NavBulletsComponent } from '../shared/nav-bullets/nav-bullets.component';
+import { FooterComponent } from '../shared/footer/footer.component';
 
 @Component({
   selector: 'app-contact',
-  imports: [HeaderComponent, TranslatePipe, TranslateDirective, NavBulletsComponent],
+  imports: [HeaderComponent, TranslatePipe, TranslateDirective, NavBulletsComponent, FooterComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
