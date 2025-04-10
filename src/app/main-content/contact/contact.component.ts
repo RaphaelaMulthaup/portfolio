b40b5f1839a25ba779051b7679909303e3de6ef9
RaphaelaMulthaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2025-04-10 14:11:28 +0200

    form

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -3,13 +3,19 @@ import { HeaderComponent } from '../shared/header/header.component';
 import { TranslatePipe, TranslateDirective } from '@ngx-translate/core';
 import { NavBulletsComponent } from '../shared/nav-bullets/nav-bullets.component';
 import { FooterComponent } from '../shared/footer/footer.component';
+import { FormComponent } from './form/form.component';
 
 @Component({
   selector: 'app-contact',
-  imports: [HeaderComponent, TranslatePipe, TranslateDirective, NavBulletsComponent, FooterComponent],
+  imports: [
+    HeaderComponent,
+    TranslatePipe,
+    TranslateDirective,
+    NavBulletsComponent,
+    FooterComponent,
+    FormComponent,
+  ],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
-export class ContactComponent {
-
-}
+export class ContactComponent {}
--- src/app/main-content/contact/form/form.component.ts
+++ src/app/main-content/contact/form/form.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-form',
-  imports: [],
+  imports: [FormsModule, TranslatePipe],
   templateUrl: './form.component.html',
-  styleUrl: './form.component.scss'
+  styleUrl: './form.component.scss',
 })
 export class FormComponent {
+  contactData = {
+    name: '',
+    email: '',
+    message: '',
+  };
 
+  onSubmit() {
+    console.log(this.contactData);
+  }
 }
