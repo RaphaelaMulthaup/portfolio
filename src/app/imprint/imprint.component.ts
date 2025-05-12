75b5990b979227906a020e85df32af544f380933
Raphaela Multhaup <raphaelamulthaup@icloud.com>
2025-05-12 17:00:20 +0200

    imprint content

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from "../main-content/shared/footer/footer.component";
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-imprint',
-  imports: [FooterComponent],
+  imports: [FooterComponent, TranslatePipe],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
