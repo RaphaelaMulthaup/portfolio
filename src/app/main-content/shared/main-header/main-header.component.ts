1542ae6bac3a638b52640e5af8dede9eb5cfb9ab
Raphaela Multhaup <raphaelamulthaup@icloud.com>
2025-05-30 18:18:43 +0200

    socialMediaButtons.component

--- src/app/main-content/shared/main-header/main-header.component.ts
+++ src/app/main-content/shared/main-header/main-header.component.ts
@@ -3,10 +3,11 @@ import { PortfolioService } from '../../../portfolio.service';
 import { LogoNameComponent } from '../logo-name/logo-name.component';
 import { MiniMenuComponent } from '../mini-menu/mini-menu.component';
 import { Router } from '@angular/router';
+import { SocialMediaButtonsComponent } from '../social-media-buttons/social-media-buttons.component';
 
 @Component({
   selector: 'app-main-header',
-  imports: [LogoNameComponent, MiniMenuComponent],
+  imports: [LogoNameComponent, MiniMenuComponent, SocialMediaButtonsComponent],
   templateUrl: './main-header.component.html',
   styleUrl: './main-header.component.scss',
 })
@@ -25,34 +26,8 @@ export class MainHeaderComponent {
   }
 
 
-  /** A boolean that indicates whether the gitHub button is hovered. */
-  gitHubIsHovered: boolean = false;
-  /** A boolean that indicates whether the linkedin button is hovered. */
-  linkedinIsHovered: boolean = false;
-  /** A boolean that indicates whether the mail button is hovered. */
-  mailIsHovered: boolean = false;
   /** A boolean that indicates whether the logo name is hovered. */
   logoNameIsHoveredImprint = false;
-
-  /**
-   * The paths to different images.
-   */
-  public images: {
-    [key: string]: { normal: string; hover: string };
-  } = {
-    gitHub: {
-      normal: 'assets/img/gitHub.png',
-      hover: 'assets/img/gitHub_hover.png',
-    },
-    linkedin: {
-      normal: 'assets/img/linkedIn.png',
-      hover: 'assets/img/linkedIn_hover.png',
-    },
-    mail: {
-      normal: 'assets/img/mail.png',
-      hover: 'assets/img/mail_hover.png',
-    },
-  };
 
   /**
    * This function navigates you to the main content component when you are on the imprint page.
---
+++ src/app/main-content/shared/social-media-buttons/social-media-buttons.component.ts
@@ -0,0 +1,41 @@
+import { Component, inject, Input } from '@angular/core';
+import { PortfolioService } from '../../../portfolio.service';
+
+@Component({
+  selector: 'app-social-media-buttons',
+  imports: [],
+  templateUrl: './social-media-buttons.component.html',
+  styleUrl: './social-media-buttons.component.scss',
+})
+export class SocialMediaButtonsComponent {
+  portfolioService = inject(PortfolioService);
+  /** This boolean indicates whether main header is on the imprint page. */
+  @Input() isImprintPage!: boolean;
+
+  /** A boolean that indicates whether the gitHub button is hovered. */
+  gitHubIsHovered: boolean = false;
+  /** A boolean that indicates whether the linkedin button is hovered. */
+  linkedinIsHovered: boolean = false;
+  /** A boolean that indicates whether the mail button is hovered. */
+  mailIsHovered: boolean = false;
+
+  /**
+   * The paths to different images.
+   */
+  public images: {
+    [key: string]: { normal: string; hover: string };
+  } = {
+    gitHub: {
+      normal: 'assets/img/gitHub.png',
+      hover: 'assets/img/gitHub_hover.png',
+    },
+    linkedin: {
+      normal: 'assets/img/linkedIn.png',
+      hover: 'assets/img/linkedIn_hover.png',
+    },
+    mail: {
+      normal: 'assets/img/mail.png',
+      hover: 'assets/img/mail_hover.png',
+    },
+  };
+}
