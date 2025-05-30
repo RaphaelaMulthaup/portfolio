20dbb8c95210bbe861cbfdbd7a65c26d9247b565
Raphaela Multhaup <raphaelamulthaup@icloud.com>
2025-05-30 15:14:23 +0200

    openOverlay() more about me

--- src/app/main-content/about-me/about-me.component.ts
+++ src/app/main-content/about-me/about-me.component.ts
@@ -7,12 +7,26 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about-me',
-  imports: [HeaderComponent, TranslatePipe, TranslateDirective, NavBulletsComponent, CommonModule],
+  imports: [
+    HeaderComponent,
+    TranslatePipe,
+    TranslateDirective,
+    NavBulletsComponent,
+    CommonModule,
+  ],
   templateUrl: './about-me.component.html',
-  styleUrl: './about-me.component.scss'
+  styleUrl: './about-me.component.scss',
 })
 export class AboutMeComponent {
   portfolioService = inject(PortfolioService);
   /** This variable indicates whether the jumping image is hovered over. */
   jumpingImgIsHoverd: boolean = false;
+  /** This variable indicates whether the overlay moreAboutMe was called on a touch screen. */
+  overlayMoreAboutMeCalled: boolean = false;
+
+  openOverlay(): void {
+    if (this.portfolioService.touchScreen) {
+      this.overlayMoreAboutMeCalled = true;
+    }
+  }
 }
