7ed4818ec36603a29ab292459c2da01f99030795
Raphaela Multhaup <raphaelamulthaup@icloud.com>
2025-06-04 09:10:31 +0200

    skills touch screen intermediate status

--- src/app/main-content/shared/header/header.component.ts
+++ src/app/main-content/shared/header/header.component.ts
@@ -1,14 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { LogoNameComponent } from '../logo-name/logo-name.component';
 import { MiniMenuComponent } from '../mini-menu/mini-menu.component';
 import { TranslatePipe, TranslateDirective } from '@ngx-translate/core';
+import { PortfolioService } from '../../../portfolio.service';
 
 @Component({
   selector: 'app-header',
-  imports: [LogoNameComponent, MiniMenuComponent, TranslatePipe, TranslateDirective],
+  imports: [
+    LogoNameComponent,
+    MiniMenuComponent,
+    TranslatePipe,
+    TranslateDirective,
+  ],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  portfolioService = inject(PortfolioService);
   @Input() headerClass: string = '';
 }
