6993f15d11254adab7981eab764bc0a564546f1e
Raphaela Multhaup <raphaelamulthaup@icloud.com>
2025-06-13 09:17:32 +0200

    Revert "feat: change of getImageSrc to access a fourth image option for social media images"
    This reverts commit 7464cd22e18ff160c5b508682cb907e2b4f94fe6.

--- src/app/main-content/shared/social-media-buttons/social-media-buttons.component.ts
+++ src/app/main-content/shared/social-media-buttons/social-media-buttons.component.ts
@@ -1,10 +1,9 @@
 import { Component, inject, Input } from '@angular/core';
 import { PortfolioService } from '../../../portfolio.service';
-import { TouchImageDirective } from '../directives/touch-image.directive';
 
 @Component({
   selector: 'app-social-media-buttons',
-  imports: [TouchImageDirective],
+  imports: [],
   templateUrl: './social-media-buttons.component.html',
   styleUrl: './social-media-buttons.component.scss',
 })
@@ -15,66 +14,31 @@ export class SocialMediaButtonsComponent {
 
   /** A boolean that indicates whether the gitHub button is hovered. */
   gitHubIsHovered: boolean = false;
-  /**
-   * Sets the state of gitHubIsHovered depending on whether the image is touched or not.
-   *
-   * @param state  - A boolean indicating whether the gitHub image is touched (true) or not (false).
-   */
-  setGitHubHovered = (state: boolean) => {
-    this.gitHubIsHovered = state;
-  };
-
   /** A boolean that indicates whether the linkedin button is hovered. */
   linkedinIsHovered: boolean = false;
-  /**
-   * Sets the state of linkedinIsHovered depending on whether the image is touched or not.
-   *
-   * @param state  - A boolean indicating whether the linkedIn image is touched (true) or not (false).
-   */
-  setLinkedInHovered = (state: boolean) => {
-    this.linkedinIsHovered = state;
-  };
-
   /** A boolean that indicates whether the mail button is hovered. */
   mailIsHovered: boolean = false;
-  /**
-   * Sets the state of mailIsHovered depending on whether the image is touched or not.
-   *
-   * @param state  - A boolean indicating whether the mail image is touched (true) or not (false).
-   */
-  setMailHovered = (state: boolean) => {
-    this.mailIsHovered = state;
-  };
 
   /**
    * The paths to different images.
    */
-  public images: {
-    [key: string]: {
-      normal: string;
-      hover: string;
-      touchScreen?: string;
-      touchScreenTouched?: string;
-    };
-  } = {
-    gitHub: {
-      normal: 'assets/img/gitHub.png',
-      hover: 'assets/img/gitHub_hover.png',
-      touchScreen: 'assets/img/gitHubBlack.png',
-      touchScreenTouched: 'assets/img/gitHub_touch.png',
-    },
-    linkedin: {
-      normal: 'assets/img/linkedIn.png',
-      hover: 'assets/img/linkedIn_hover.png',
-      touchScreen: 'assets/img/linkedinBlack.png',
-      touchScreenTouched: 'assets/img/linkedIn_touch.png',
-    },
-    mail: {
-      normal: 'assets/img/mail.png',
-      hover: 'assets/img/mail_hover.png',
-      touchScreen: 'assets/img/mailBlack.png',
-      touchScreenTouched: 'assets/img/mail_touch.png',
-    },
-  };
-  cd: any;
+public images: {
+  [key: string]: { normal: string; hover: string; touch?: string };
+} = {
+  gitHub: {
+    normal: 'assets/img/gitHub.png',
+    hover: 'assets/img/gitHub_hover.png',
+    touch: 'assets/img/gitHubBlack.png',
+  },
+  linkedin: {
+    normal: 'assets/img/linkedIn.png',
+    hover: 'assets/img/linkedIn_hover.png',
+    touch: 'assets/img/linkedinBlack.png',
+  },
+  mail: {
+    normal: 'assets/img/mail.png',
+    hover: 'assets/img/mail_hover.png',
+    touch: 'assets/img/mailBlack.png',
+  },
+};
 }
