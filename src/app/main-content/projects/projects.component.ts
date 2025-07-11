64744cd5fcf2e7a0ad6be7c92bb84a9e390b2e25
Raphaela Multhaup <raphaelamulthaup@icloud.com>
2025-07-11 10:45:41 +0200

    fix: large images compressed

--- src/app/main-content/projects/projects.component.ts
+++ src/app/main-content/projects/projects.component.ts
@@ -48,7 +48,7 @@ export class ProjectsComponent {
   /** The data of the projects. */
   dataProjects: { [key: string]: ProjectData } = {
     join: {
-      img: 'assets/img/join.png',
+      img: 'assets/img/join.jpg',
       bGroundColor: 'bgYellow',
       colorHexagon: 'hexagonOrange',
       name: 'Join',
